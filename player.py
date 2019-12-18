68e937dc2349efec770ba502d61baf55b17e767b
Arjun Darbha <adarbha@mtu.edu>
2019-12-18 23:33:48 +0530

    Game and player modules for creating base objects

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player:
+'''Template of a player class for creating a new player'''
+
+def __init__(self, name, marker):
+    '''name - Unique name of the player
+       marker - Unique marker for this player. Select from X/O'''
+       self.name = name
+       self.marker = marker
+
+def __repr__(self):
+    return "Player name: {} \n \
+            Marker: {}".format(self.name, self.marker)
+
+
+    
