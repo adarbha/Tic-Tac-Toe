1cda6869f90ad155547f33bb16d8c39f9a047b22
Arjun Darbha <adarbha@mtu.edu>
2019-12-20 00:09:35 +0530

    basic setup.py info

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'tictactoe-adarbha',
+      version = '0.1',
+      description = 'Pieces for creating a TicTacToe Game',
+      packages = ['tictactoe-adarbha'],
+      zip_safe = False)
